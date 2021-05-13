1dec31b25bc692ec48cca55c3333ca4f745aceae
Serra Yilmaz <serrayilmaz@sabanciuniv.edu>
2021-05-13 14:41:25 +0300

    Add files via upload

---
+++ Project/match.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Football League</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/styles.css">
+
+  <script src="https://kit.fontawesome.com/fbad311eca.js" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/1b93a4ba68.js" crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+  <link href="images/favicon (2).ico" rel="icon" type="image/x-icon">
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+  <section id="title">
+    <div class="container">  <!---fluid-->
+      <nav class="navbar navbar-dark navbar-expand-lg" style="background-color: #111f4d;">
+        <a class="navbar-brand" href="homepage.php">Football League Database</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="navbar-item">
+              <a class="nav-link" href="team.php">Team</a>
+            </li>
+            <li class="navbar-item">
+              <a class="nav-link" href="player.php">Player</a>
+            </li>
+            <li class="navbar-item">
+              <a class="nav-link" href="coach.php">Coach</a>
+            </li>
+            <li class="navbar-item">
+              <a class="nav-link" href="coach_manages_team.php">Coach Manages Team</a>
+            </li>
+            <li class="navbar-item">
+              <a class="nav-link" href="player_in_team.php">Player In Team</a>
+            </li>
+            <li class="navbar-item">
+              <a class="nav-link" href="match.php">Match</a>
+            </li>
+            <li class="navbar-item">
+              <a class="nav-link" href="stadium.php">Stadium</a>
+            </li>
+            <li class="navbar-item">
+              <a class="nav-link" href="standing.php">Standing</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <!-- Title -->
+
+      <div class="row">
+        <div class="col-lg-1">
+
+        </div>
+        <div class="col-lg-3 buttons">
+          <br><br><br><br>
+            <form  method="POST" >
+              <input type="text" class="form-control" id="matchID" name="matchID" placeholder="Match ID" required><br>
+              <input type="text" class="form-control" id="awayTeamID" name="awayTeamID" placeholder="Away Team ID" required><br>
+              <input type="text" class="form-control" id="homeTeamID" name="homeTeamID" placeholder="Home Team ID" required><br>
+              <input type="text" class="form-control" id="stadiumID	" name="stadiumID	" placeholder="Stadium ID	" required><br>
+              <input type="text" class="form-control" id="refereeName" name="refereeName" placeholder="Referee Name" required><br>
+              <input type="text" class="form-control" id="matchDate" name="matchDate" placeholder="Match Date" required><br>
+              <input type="text" class="form-control" id="awayScore" name="awayScore" placeholder="Away Score" required><br>
+              <input type="text" class="form-control" id="homeScore	" name="homeScore	" placeholder="Home Score	" required><br>
+              <button class="btn btn-outline-secondary btn-md">Insert</button>
+            </form>
+        </div>
+        <div class="col-lg-3">
+
+        </div>
+        <div class="col-lg-5">
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+
+  <!-- Footer -->
+
+  <footer id="footer">
+
+    <p></p>
+
+  </footer>
+
+</body>
+
+</html>
